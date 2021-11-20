df627e8c70b41058d11ee8d19e8bb79c3cf7b247
DIEGO ARMANDO BAES OSORIO <diegoarmandobaes4@gmail.com>
2021-11-20 05:14:41 +0000

    Update POLINOMIOS2.m

--- POLINOMIOS2.m
+++ POLINOMIOS2.m
@@ -1,3 +1,12 @@
+%Octave script
+%Title              :Raices algebraicas plinomiales
+%Description        :Determinar las raices polinomiales 
+%%Authores          :Diego Armando Baes Osorio
+%Date               :20211911
+%Version            :8
+%Usage              :Octave>path/Valor Real 
+%                   :Requiere de aplicacion octave, usar su linea de comando
+
 P = [2,4,1]
 
 roots(P)
