9fd2a70cf953ae8414fedf14b4f3c96c966dff71
DIEGO ARMANDO BAES OSORIO <diegoarmandobaes4@gmail.com>
2021-11-20 05:46:25 +0000

    Upload New File

---
+++ RACIONALES2.m
@@ -0,0 +1,13 @@
+%Octave script
+%Title              :Raices algebraicas plinomiales
+%Description        :Determinar las raices polinomiales 
+%%Authores          :Diego Armando Baes Osorio
+%Date               :20211911
+%Version            :8
+%Usage              :Octave>path/Valor Real 
+%                   :Requiere de aplicacion octave, usar su linea de comando
+
+P = [1,2,1,-2]
+
+roots(P)
+ezplot("x +2")("x -2") 
